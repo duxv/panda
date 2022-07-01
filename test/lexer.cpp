292e4ca38ed82851062bc9c1022eaa320b0169fb
david <mrcakee17@gmail.com>
2022-07-01 15:56:50 +0300

    solved some bugs & better lexer testing

--- test/lexer.cpp
+++ test/lexer.cpp
@@ -4,10 +4,12 @@
 
 int main() {
     int i = 0;
-    std::string input = "let x = y\n"
-                        "break // comments should be ignored\n"
-                        "/* multiline comments\n"
-                        "should be ignored as \n\n\nwell*/";
+    std::string input = "let x = 11\n\n"
+                        "let y: i32 = 1.29\n"
+                        "if x == y && x + y < 200 || 1 == 1 {\n"
+                        "println(\"hello world\")\n"
+                        "for x in arr { println(\"Found x right here {}\", x)}\n";
+                        "/*Multiline string\n\n*/";
     Lexer lex(input, [](std::size_t pos, const char* msg) {
         printf("POS: %ld: Error %s\n", pos, msg);
         std::exit(0);
@@ -20,9 +22,48 @@ int main() {
         {Token::LET, "let"},
         {Token::IDENT, "x"},
         {Token::ASSIGN, ""},
-        {Token::IDENT, "y"},
+        {Token::INT, "11"},
         {Token::NEWLINE, ""},
-        {Token::BREAK, "break"},
+        {Token::LET, "let"},
+        {Token::IDENT, "y"},
+        {Token::COLON, ""},
+        {Token::IDENT, "i32"},
+        {Token::ASSIGN, ""},
+        {Token::FLOAT,"1.29"},
+        {Token::NEWLINE, ""},
+        {Token::IF, "if"},
+        {Token::IDENT, "x"},
+        {Token::EQUAL, ""},
+        {Token::IDENT, "y"},
+        {Token::AND, ""},
+        {Token::IDENT, "x"},
+        {Token::ADD, ""},
+        {Token::IDENT, "y"},
+        {Token::LESS, ""},
+        {Token::INT, "200"},
+        {Token::OR, ""},
+        {Token::INT, "1"},
+        {Token::EQUAL, ""},
+        {Token::INT, "1"},
+        {Token::LBRACE, ""},
+        {Token::NEWLINE, ""},
+        {Token::IDENT, "println"},
+        {Token::LPAREN, ""},
+        {Token::STRING, "hello world"},
+        {Token::RPAREN, ""},
+        {Token::NEWLINE, ""},
+        {Token::FOR, "for"},
+        {Token::IDENT, "x"},
+        {Token::IN, "in"},
+        {Token::IDENT, "arr"},
+        {Token::LBRACE, ""},
+        {Token::IDENT, "println"},
+        {Token::LPAREN, ""},
+        {Token::STRING, "Found x right here {}"},
+        {Token::COMMA, ""},
+        {Token::IDENT, "x"},
+        {Token::RPAREN, ""},
+        {Token::RBRACE, ""},
         {Token::NEWLINE, ""},
         {Token::ENDMARKER, ""},
     };
@@ -30,11 +71,12 @@ int main() {
         auto lex_tok = lex.nextToken();
         if (lex_tok.first != test.tok) {
             std::cout << "[ERROR] tokens don't match: " << "test number " << i << ": ";
-            std::cout << "want " << test.tok << " got " << lex_tok.first << '\n';
+            std::cout << "want " << test.tok << " got '" << lex_tok.first << "'\n";
             return 1;
         } else if (lex_tok.second != test.literal) {
             std::cout << "[ERROR] literals don't match: " << "test number " << i << ": ";
-            std::cout << "want " << test.literal << " got " << lex_tok.second << '\n'; 
+            std::cout << "want " << test.literal << " got '" << lex_tok.second << "'\n"; 
+            return 1;
         }
         i++; 
     }
